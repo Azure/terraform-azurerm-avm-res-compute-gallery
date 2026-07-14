e6e836fcb42752aa3f314f79a869917687c3ca74
Jared Holgate <jaredholgate@microsoft.com>
2026-07-14 08:50:59 +0100

    pre-commit

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ resource "azurerm_shared_image_gallery" "this" {
       }
     }
   }
+
   dynamic "timeouts" {
     for_each = var.timeouts != null ? [var.timeouts] : []
 
@@ -69,6 +70,7 @@ resource "azurerm_shared_image" "this" {
     publisher = each.value.identifier.publisher
     sku       = each.value.identifier.sku
   }
+
   ## Optional Inputs
   dynamic "purchase_plan" {
     for_each = each.value.purchase_plan != null ? [each.value.purchase_plan] : []
